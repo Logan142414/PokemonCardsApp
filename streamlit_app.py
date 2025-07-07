6ce903f49dd09ef4024379b00835c67b0a1ccaa8
Logan Laszewski <logan.laszewski@comcast.net>
2025-07-06 22:11:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,7 +105,7 @@ def load_data():
 col1, col2, col3 = st.columns([1, 6, 1])
 
 with col1:
-    st.image("PikaRunning.png", use_column_width=True)
+    st.image("PikaRunning.png", use_container_width = True)
 
 with col2:
     st.markdown(
